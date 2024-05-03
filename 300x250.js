bc4ace7e024ef07bcd02edae24a2b7cda32c5c27
fsinagala <fsinagala@gmail.com>
2024-05-03 12:46:30 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '967964a35feaad0d9893b545abd8a15d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/967964a35feaad0d9893b545abd8a15d/invoke.js"></script>
