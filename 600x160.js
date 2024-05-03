402968c49a7a1f36d2a5a76eb41cdf81dc728c60
fsinagala <fsinagala@gmail.com>
2024-05-03 12:49:40 +0700

    600x160.js

---
+++ 600x160.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '36a4b4d8b6bd649ab1e3e9e4d4b8c5ce',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/36a4b4d8b6bd649ab1e3e9e4d4b8c5ce/invoke.js"></script>
