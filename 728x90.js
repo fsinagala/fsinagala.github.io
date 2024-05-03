bf8978cee12d6fba285e92020279f276cc5c1f56
fsinagala <fsinagala@gmail.com>
2024-05-03 12:48:59 +0700

    728x90.js

---
+++ 728x90.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b93f01181d61e05ae57b1e2623ae9194',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/b93f01181d61e05ae57b1e2623ae9194/invoke.js"></script>
